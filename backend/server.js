66b20a252e70cf2d867689a9b6ba87468d43e2c7
Aladdin Elshamy <aladdinelshamy@gmail.com>
2025-12-25 15:34:10 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import appRoutes from '../routes/appRoutes.js';
+import appRoutes from './routes/appRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
+    origin:'*'
 }));
-
-app.options('*', cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-app.use('/', appRoutes);
+app.use('/api', appRoutes);
 app.use((req, res) => {
     res.status(404).json({ error: 'Path Not Found' });
 });
